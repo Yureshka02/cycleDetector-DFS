de11867a59ece0c31c28cbbc21b1ef39b041ac43
WATUTHANTHIRIGE LASHANE YURESHKA ALWIS <133086262+Yureshka02@users.noreply.github.com>
2024-09-21 03:09:53 +0530

    implemented the Undirected graphs with cycles and without cycles

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,21 @@ public class Main {
         directedGraph2.addEdge(2, 3);
         directedGraph2.detectCycle(); // Should not detect a cycle
 
+        // Undirected Graph Example
+        System.out.println("\nUndirected Graph:");
+        Graph undirectedGraph = new Graph(5, false); // Creating an undirected graph
+        undirectedGraph.addEdge(0, 1);
+        undirectedGraph.addEdge(1, 2);
+        undirectedGraph.addEdge(2, 0); // This forms a cycle
+        undirectedGraph.addEdge(1, 3);
+        undirectedGraph.addEdge(3, 4);
+        undirectedGraph.detectCycle(); // Should detect a cycle
 
+        Graph undirectedGraph2 = new Graph(5, false);
+        undirectedGraph2.addEdge(0, 1);
+        undirectedGraph2.addEdge(1, 2);
+        undirectedGraph2.addEdge(1, 3);
+        undirectedGraph2.addEdge(3, 4);
+        undirectedGraph2.detectCycle(); // Should not detect a cycle
     }
 }
